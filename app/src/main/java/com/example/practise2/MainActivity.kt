3d0af5e8e4bec217a42b09236ef73e40468c9d2e
Riyal Riyan <riyel1422@gmail.com>
2024-02-18 17:41:36 +0600

    all done

--- app/src/main/java/com/example/practise2/MainActivity.kt
+++ app/src/main/java/com/example/practise2/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.practise2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,16 +13,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Yellow
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.practise2.ui.theme.Practise2Theme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    DollarCounter()
                 }
             }
         }
@@ -39,17 +45,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun dollerCounter()
-{
-    Column ( modifier = Modifier.fillMaxSize()) {
-       Text(text = "$400", style = MaterialTheme.typography.bodyMedium)
+fun DollarCounter() {
+    val counter = remember {
+        mutableIntStateOf(1)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "$${counter.intValue*100}", style = MaterialTheme.typography.bodyMedium, fontSize = 30.sp)
         Spacer(modifier = Modifier.height(180.dp))
+        CustomButton{
+            counter.intValue++
+        }
 
     }
 }
-@Composable
-fun CustomButton(){
-    Card (modifier = Modifier.size(120.dp), shape = CircleShape, CardColors = Color.Yellow){
 
+@Composable
+fun CustomButton(onClick: ()-> Unit) {
+    Card(
+        modifier = Modifier
+            .size(120.dp)
+            .clickable {
+onClick.invoke()
+            },
+        shape = CircleShape,
+        CardDefaults.cardColors(containerColor = Yellow),
+    ) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text(text = "Tap", fontSize = 30.sp, fontWeight = FontWeight.ExtraBold)
+        }
     }
 }
